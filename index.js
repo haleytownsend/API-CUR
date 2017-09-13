a8946bfe2eb2525a4a02f2ffabc68c96efdde1f7
Haley Townsend <haleyrk@hotmail.com>
2017-09-13 10:09:19 -0500

    currency bar is now working

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ var API_BASE_URL ="https://api.fixer.io/latest";
 var CUR_ONE= "USD"
 var CUR_TWO= "EUR"
 
-var amt = "1"
-
 var CUR_BAR_BASE= "USD"
 var ALL_CURRENCIES = {
   currency: [
@@ -19,10 +17,6 @@ var ALL_CURRENCIES = {
 
 $(document).ready(function() {
   console.log("ready");
-
-  var a=([ALL_CURRENCIES.currency[1]])
-  console.log(a)
-  var b=(searchAPI("USD", a))
 
   $(`#currencyOne`).change(function() {
     var el = document.getElementById("currencyOne");
@@ -46,7 +40,35 @@ $(document).ready(function() {
       displayResults(data.rates[CUR_TWO])
     })
   });
-});
+
+//currencyBar
+  searchAPI("USD", "CAD")
+  .then(function(data) {
+    $('#CAD').html(data.rates["CAD"].toFixed(2))
+  console.log(data.rates["CAD"].toFixed(2))
+  })
+  searchAPI("USD", "EUR")
+  .then(function(data) {
+    $('#EUR').html(data.rates["EUR"].toFixed(2))
+  })
+  searchAPI("USD", "BRL")
+  .then(function(data) {
+    $('#BRL').html(data.rates["BRL"].toFixed(2))
+  })
+  searchAPI("USD", "GBP")
+  .then(function(data) {
+    $('#GBP').html(data.rates["GBP"].toFixed(2))
+  })
+  searchAPI("USD", "JPY")
+  .then(function(data) {
+    $('#JPY').html(data.rates["JPY"].toFixed(2))
+  })
+  searchAPI("USD", "MXN")
+  .then(function(data) {
+    $('#MXN').html(data.rates["MXN"].toFixed(2))
+  })
+})
+
 
 function searchAPI(base, symbols) {
   return $.ajax(API_BASE_URL, {
